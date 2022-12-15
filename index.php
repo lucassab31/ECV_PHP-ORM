2479624226d10611bd106fcf5c618b4a8615312a
Lucas <luqui31@gmail.com>
2022-12-15 15:09:28 +0100

    Structure

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-    
+    if (!isset($_GET['page']))
+        $sPage = 'home';
+    else
+        $sPage = $_GET['page'];
+
+    if ($sPage == 'home') {
+        require_once('./Controllers/PostController.php');
+        $tPosts = PostController::getAll();
+        require_once('./Views/posts/posts.php');
+    }
 ?>
