f58a1b7a9ac0c2080c59b8b55c1a4d27e14a7e37
Valentin Muller <valmuller14@gmail.com>
2022-12-29 16:18:04 +0100

    creating a method to save a post

--- Controllers/PostController.php
+++ Controllers/PostController.php
@@ -9,5 +9,15 @@
         public static function getPost($iId) {
             return Post::getOne($iId);
         }
+
+        public static function store() {
+            $oPost = new Post();
+            $oPost->title = $_POST['title'];
+            $oPost->content = $_POST['content'];
+            $oPost->user = $_POST['user'];
+            $oPost->created_at = date('Y-m-d H:i:s');
+            $oPost->save();
+            return $oPost;
+        }
     }
 ?>
