ff1227e8110783a0ba76b8fd8e6c6f8700b6c576
Valentin Muller <valmuller14@gmail.com>
2023-01-05 10:19:40 +0100

    login ingoing

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController
             return null;
         }
         if (md5($sPassword)== $oUser->password) {
-            $_SESSION['user'] = $oUser;
+            $_SESSION['user_id'] = $oUser->user_id;
             return $oUser;
         }
         return null;
@@ -27,7 +27,7 @@ class UserController
         $oUser->email = $sEmail;
         $oUser->password = md5($sPassword);
         $oUser->save();
-        $_SESSION['user'] = $oUser;
+        $_SESSION['user_id'] = $oUser->user_id;
         return $oUser;
     }
 
