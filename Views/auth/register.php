327ef80ddce0b12490c263ccab723134e29244df
Lucas <luqui31@gmail.com>
2023-01-05 10:40:14 +0100

    regsiter email used

--- Views/auth/register.php
+++ Views/auth/register.php
@@ -1,11 +1,11 @@
 <div class="row justify-content-center">
     <div class="col-md-6">
-        <form action="?page=user-store" method="POST">
+        <form action="?page=user-store" method="POST" class="i">
             <div class="mb-3">
                 <label for="user" class="form-label">Email</label>
                 <input type="text" class="form-control" id="email" name="email" required>
                 <?php if (isset($sError)) { ?> 
-                    <span class="invalid-feedback"><?= $sError ?></span>
+                    <span class="text-danger"><?= $sError ?></span>
                 <?php } ?>
             </div>
             <div class="mb-3">
